bc28946501a47c7d78d2310828d3f5ecae744bf9
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2023-09-24 15:32:25 +0200

    feat: added coin piles

--- DynamicStoragePiles/DynamicStoragePiles.cs
+++ DynamicStoragePiles/DynamicStoragePiles.cs
@@ -32,6 +32,7 @@ namespace DynamicStoragePiles {
             AddPiece("MS_container_coal_pile", "Coal");
             AddPiece("MS_container_yggdrasil_wood_stack", "YggdrasilWood");
             AddPiece("MS_container_blackmarble_pile", "BlackMarble");
+            AddPiece("MS_container_coin_pile", "Coins");
 
             PieceManager.OnPiecesRegistered += OnPiecesRegistered;
 
