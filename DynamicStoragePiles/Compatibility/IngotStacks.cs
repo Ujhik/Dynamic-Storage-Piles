d41e430f10cf547659f71475ac2d3e860bb87afe
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2023-10-24 11:50:16 +0200

    feat: added missing ingot stacks

--- DynamicStoragePiles/Compatibility/IngotStacks.cs
+++ DynamicStoragePiles/Compatibility/IngotStacks.cs
@@ -9,6 +9,12 @@ namespace DynamicStoragePiles.Compatibility {
         public static void Init() {
             assetBundle = GetAssetBundle();
             ConvertToPiece("ingot_copper_stack", "vfx_copper_stack_destroyed", "MS_IngotStacks_Copper", "Copper");
+            ConvertToPiece("ingot_tin_stack", "vfx_tin_stack_destroyed", "MS_IngotStacks_Tin", "Tin");
+            ConvertToPiece("ingot_bronze_stack", "vfx_bronze_stack_destroyed", "MS_IngotStacks_Bronze", "Bronze");
+            ConvertToPiece("ingot_iron_stack", "vfx_iron_stack_destroyed", "MS_IngotStacks_Iron", "Iron");
+            ConvertToPiece("ingot_silver_stack", "vfx_silver_stack_destroyed", "MS_IngotStacks_Silver", "Silver");
+            ConvertToPiece("ingot_blackmetal_stack", "vfx_blackmetal_stack_destroyed", "MS_IngotStacks_Blackmetal", "BlackMetal");
+            ConvertToPiece("ingot_flametal_stack", "vfx_flametal_stack_destroyed", "MS_IngotStacks_Flametal", "Flametal");
         }
 
         private static void ConvertToPiece(string baseAssetName, string vfxName, string newPrefabName, string resource) {
