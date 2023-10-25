98f73654adea0800e3b568e6832830d682ea46aa
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2023-10-25 16:46:17 +0200

    Release 0.4.0

--- DynamicStoragePiles/DynamicStoragePiles.cs
+++ DynamicStoragePiles/DynamicStoragePiles.cs
@@ -17,7 +17,7 @@ namespace DynamicStoragePiles {
     internal class DynamicStoragePiles : BaseUnityPlugin {
         public const string PluginName = "DynamicStoragePiles";
         public const string PluginGuid = "com.maxsch.valheim.DynamicStoragePiles";
-        public const string PluginVersion = "0.3.0";
+        public const string PluginVersion = "0.4.0";
 
         public static DynamicStoragePiles Instance { get; private set; }
         public static Harmony harmony;
